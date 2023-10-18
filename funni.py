ab4d7f323fbd4afe71894961f23af8711c67e142
mynamescheff <mihu@int.pl>
2023-10-18 09:36:31 +0200

    updated

--- funni.py
+++ funni.py
@@ -6,13 +6,13 @@ def move_away(event):
     x, y = root.winfo_x(), root.winfo_y()
     
     # Calculate the new position, making sure the window stays on the screen
-    new_x = max(0, min(root.winfo_screenwidth() - root.winfo_width(), x + random.randint(-100, 100)))
-    new_y = max(0, min(root.winfo_screenheight() - root.winfo_height(), y + random.randint(-100, 100)))
+    new_x = max(0, min(root.winfo_screenwidth() - root.winfo_width(), x + random.randint(-50, 50)))
+    new_y = max(0, min(root.winfo_screenheight() - root.winfo_height(), y + random.randint(-50, 50)))
     
     # Move the window to the new position
     root.geometry(f"+{new_x}+{new_y}")
 
-def close_window():
+def close_window(event):
     root.destroy()
 
 root = tk.Tk()
