bee7add18187a0904ac41db320043bdb3b1a28a2
mynamescheff <mihu@int.pl>
2023-11-11 11:13:14 +0100

    updated

--- poker/poker.py
+++ poker/poker.py
@@ -11,20 +11,40 @@ def poker_hand(hand):
     values = [card[0] for card in hand]
     suits = [card[1] for card in hand]
 
+    # Check for pairs, three of a kind, and four of a kind
+    value_counts = {value: values.count(value) for value in set(values)}
+    num_pairs = sum(count == 2 for count in value_counts.values())
+    num_three_of_a_kind = sum(count == 3 for count in value_counts.values())
+    num_four_of_a_kind = sum(count == 4 for count in value_counts.values())
+
+    # Check for straight and royal straight
+    is_royal_straight = all(value in values for value in (10, 11, 12, 13, 14))
+    is_normal_straight = all(value in values for value in (9, 10, 11, 12, 13))
+
     # Check for poker hand combinations
     if all(value == values[0] for value in values):
         return "Royal Flush"
-    elif len(set(values)) == 2:
-        if values.count(values[0]) in (1, 4):
-            return "Four of a Kind"
-        else:
-            return "Full House"
+    elif num_four_of_a_kind == 1:
+        return "Four of a Kind"
+    elif num_three_of_a_kind == 1 and num_pairs == 1:
+        return "Full House"
     elif len(set(suits)) == 1:
-        return "Flush"
-    elif all(value in values for value in (9, 10, 11, 12, 13)) and len(set(suits)) == 1:
-        return "Straight Flush"
-    elif all(value in values for value in (9, 10, 11, 12, 13)):
+        if is_royal_straight:
+            return "Royal Straight Flush"
+        elif is_normal_straight:
+            return "Straight Flush"
+        else:
+            return "Flush"
+    elif is_royal_straight:
+        return "Royal Straight"
+    elif is_normal_straight:
         return "Straight"
+    elif num_three_of_a_kind == 1:
+        return "Three of a Kind"
+    elif num_pairs == 2:
+        return "Two Pairs"
+    elif num_pairs == 1:
+        return "Pair"
     else:
         return "High Card"
 
@@ -35,8 +55,10 @@ deck = [(card_names[rank], color) for rank in card_names for color in colors]
 def draw_hand(deck, num_cards=5):
     return random.sample(deck, num_cards)
 
-# Generate and print a random hand
-hand = draw_hand(deck)
-for card in hand:
-    print(f'{card[0]} of {card[1]}')
-print(f"Poker Hand: {poker_hand(hand)}")
+# Draw and display hands five times
+for _ in range(5):
+    hand = draw_hand(deck)
+    for card in hand:
+        print(f'{card[0]} of {card[1]}')
+    print(f"Poker Hand: {poker_hand(hand)}")
+    print("------")
